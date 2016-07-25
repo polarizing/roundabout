dc8a29eaf7171d98cfe76f64d8e3e703d843d5db
HarrisonLo <harrison0723@gmail.com>
2016-07-25 19:45:58 -0400

    association finished

--- server/db/index.js
+++ server/db/index.js
@@ -3,4 +3,20 @@ var db = require('./_db');
 module.exports = db;
 
 var User = require('./models/user');
+var Tour = require('./models/tour');
+var Booking = require('./models/booking');
+var Review = require('./models/review');
 
+User.hasMany('Tour');
+User.hasMany('Booking');
+User.hasMany('Review');
+
+Tour.hasOne('Booking');
+Tour.hasMany('Review');
+Tour.belongsTo('User');
+
+Booking.belongsTo('Tour');
+Booking.belongsTo('User');
+
+Review.belongsTo('Tour');
+Review.belongsTo('User')
