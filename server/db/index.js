02529d7fbe53e76b323b6bc7a9dd44084e66f332
HarrisonLo <harrison0723@gmail.com>
2016-07-25 20:52:15 -0400

    fix syntax error

--- server/db/index.js
+++ server/db/index.js
@@ -8,19 +8,19 @@ var Booking = require('./models/booking');
 var Review = require('./models/review');
 var Tag = require('./models/tag');
 
-User.hasMany('Tour');
-User.hasMany('Booking');
-User.hasMany('Review');
+User.hasMany(Tour);
+User.hasMany(Booking);
+User.hasMany(Review);
 
-Tour.hasMany('Tag')
-Tour.hasOne('Booking');
-Tour.hasOne('Review');
-Tour.belongsTo('User', { as: 'Guide'});
+Tour.hasMany(Tag)
+Tour.hasOne(Booking);
+Tour.hasOne(Review);
+Tour.belongsTo(User, { as: 'Guide'});
 
-Booking.belongsTo('Tour');
-Booking.belongsTo('User');
+Booking.belongsTo(Tour);
+Booking.belongsTo(User);
 
-Review.belongsTo('Tour');
-Review.belongsTo('User');
+Review.belongsTo(Tour);
+Review.belongsTo(User);
 
 Tag.belongsToMany(Tour);
