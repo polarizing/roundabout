bf9bb937346e6c53d077a05e1a9bb7f218a9e634
Jonathan Ligh <JonathankLigh@gmail.com>
2016-07-26 16:35:38 -0400

    tours routes ready for inspection

---
+++ server/app/routes/tours/index.js
@@ -0,0 +1,55 @@
+'use strict';
+var router = require('express').Router();
+module.exports = router;
+var _ = require('lodash');
+var Tour = require('../../db/models/tour');
+
+router.param('id', function (req, res, next, id) {
+    Tour.findById(id)
+    .then(function (tour) {
+        req.requestedTour = tour;
+        next();
+    })
+    .catch(next);
+});
+
+router.get('/:id', function(req, res, next) {
+    req.requestedTour.reload()
+    .then(function(tour) {
+        res.send(tour);
+    })
+    .catch(next);
+});
+
+router.post('/:id', function(req, res, next) {
+    Tour.create(req.body)
+    .then(function(tour) {
+        res.status(201);
+        res.send(tour);
+    })
+    .catch(next);
+});
+
+router.put('/:id', function(req, res, next) {
+    req.requestedTour.update(req.body)
+    .then(function (tour) {
+        res.send(tour);
+    })
+    .catch(next);
+});
+
+router.delete('/:id', function(req, res, next) {
+    req.requestedTour.destroy()
+    .then(function () {
+        res.status(204).end();
+    })
+    .catch(next);
+});
+
+router.get('/', function(req, res, next) {
+    Tour.findAll()
+    .then(function(tours) {
+        res.send(tours)
+    })
+    .catch(next);
+});
