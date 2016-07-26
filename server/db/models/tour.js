0f9a676c914c94dad9f8ce7a1cee94c82a2be7d6
ishaan381 <in2187@columbia.edu>
2016-07-26 14:41:30 -0400

    tour udated

--- server/db/models/tour.js
+++ server/db/models/tour.js
@@ -4,57 +4,59 @@ var Sequelize = require('sequelize');
 
 var db = require('../_db');
 
-db.define('tour', {
-  title: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
+module.exports = db.define('tour', {
+ title: {
+   type: Sequelize.STRING,
+   allowNull: false
+ },
 
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
+ description: {
+   type: Sequelize.STRING,
+   allowNull: false
+ },
 
-  tags: {
-    type: Sequelize.ARRAY(Sequelize.STRING)
-  },
+ tags: {
+   type: Sequelize.ARRAY(Sequelize.STRING)
+ },
 
-  price: {
-    type: Sequelize.FLOAT,
-    allowNull: false
-  },
+ price: {
+   type: Sequelize.FLOAT,
+   allowNull: false
+ },
 
-  tod: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
+ tod: {
+   type: Sequelize.STRING,
+   allowNull: false
+ },
 
-  duration: {
-    type: Sequelize.INTEGER,
-    defaultValue: 60
-  },
+ duration: {
+   type: Sequelize.INTEGER,
+   defaultValue: 60
+ },
 
-  book_by: {
-    type: Sequelize.DATE,
-    defaultValue: (new Date()).setHours((new Date()).getHours() + 3)
-  }
+ expire_in: {
+   type: Sequelize.INTEGER,
+   defaultValue: 180
+ },
 
-  //guideId will be set when we do tour.belongsTo(user, as guide)
-
-
- }, {
-
-  getterMethods: {
-    isActive: function() {
-      return Date.now() < this.book_by
-    }
-  }
+ book_by: {
+   type: Sequelize.DATE
 }
 
+ //guideId will be set when we do tour.belongsTo(user, as guide)
 
 
- )
-
-
-module.exports = db;
-
+}, {
+ hooks: {
+   afterValidate: function(tour, options) {
+     var currTime = new Date();
+     tour.book_by = new Date(currTime.getTime() + tour.expire_in*60000);
+   }
+ },
+ getterMethods: {
+   isActive: function() {
+     return Date.now() < this.book_by.getTime()
+   }
+ }
+}
+)
