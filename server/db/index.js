f39de6a626acd4ecb798ad60cc792315d94f9516
HarrisonLo <harrison0723@gmail.com>
2016-07-25 20:19:48 -0400

    add tags to associations

--- server/db/index.js
+++ server/db/index.js
@@ -6,11 +6,13 @@ var User = require('./models/user');
 var Tour = require('./models/tour');
 var Booking = require('./models/booking');
 var Review = require('./models/review');
+var Tag = require('./models/tag');
 
 User.hasMany('Tour');
 User.hasMany('Booking');
 User.hasMany('Review');
 
+Tour.hasMany('Tag')
 Tour.hasOne('Booking');
 Tour.hasOne('Review');
 Tour.belongsTo('User', { as: 'Guide'});
@@ -19,4 +21,6 @@ Booking.belongsTo('Tour');
 Booking.belongsTo('User');
 
 Review.belongsTo('Tour');
-Review.belongsTo('User')
+Review.belongsTo('User');
+
+Tag.belongsToMany(Tour);
