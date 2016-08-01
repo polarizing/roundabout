61d399bf37cf874ebbe216fb29dd7f928d510062
Jonathan Ligh <JonathankLigh@gmail.com>
2016-08-01 16:03:02 -0400

    imcomplete funcitonality, but sign up is complete

--- browser/js/users/profile/edit/users.profile.edit.controller.js
+++ browser/js/users/profile/edit/users.profile.edit.controller.js
@@ -1,5 +1,17 @@
 'use strict';
 
-app.controller('UserProfileEdit', function ($scope, $state, Session, $kookies, $log) {
-
+app.controller('UserProfileEdit', function ($scope, $state, Session, $kookies, User, $log) {
+	$scope.user = {
+		id: Session.user.id,
+		name: Session.user.name,
+		photo: Session.user.photo,
+		email: Session.user.email,
+		phone: Session.user.phone,
+		password: Session.user.password
+	}
+	console.log(Session.user);
+	$scope.editUser = function(){
+		// if(User.encryptPassword(Session.user.oldpassword, Session.user.salt) === User.password){
+			User.edit($scope.user)
+		}
 });
