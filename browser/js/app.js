df453d2c7ec7b97c8c34cc97e92c8af3628ce928
ishaan381 <in2187@columbia.edu>
2016-08-02 10:47:32 -0400

    fixed home state

--- browser/js/app.js
+++ browser/js/app.js
@@ -81,5 +81,5 @@ app.run(function ($rootScope, AuthService, $state) {
       $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
             console.error('Error transitioning from "' + fromState.name + '" to "' + toState.name + '":', error);
           });
-    
+
 });
